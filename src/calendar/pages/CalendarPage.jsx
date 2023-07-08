a813d124772228e05bc8ae8293a570fe8e726d85
Guillermo Sandoval <tinta.mas.toner.mzt@gmail.com>
2023-07-07 17:28:33 -0700

    CalendarPage.jsx Paquete calendario implementado en proyecto

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,10 +1,50 @@
-import { NavBar } from "../components/NavBar"
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import enUS from 'date-fns/locale/en-US';
+import { addHours, parse, format, startOfWeek, getDay } from 'date-fns';
+
+import { NavBar } from '../components/NavBar';
 
 
 export const CalendarPage = () => {
+
+  const locales = {
+    'en-US': enUS,
+  };
+
+  const localizer = dateFnsLocalizer({
+    format,
+    parse,
+    startOfWeek,
+    getDay,
+    locales,
+  });
+
+  const events = [{
+    title: 'Cumpleaños del jefe',
+    notes: 'Hay que comprar el pastel',
+    start: new Date(),
+    end: addHours( new Date(), 2),
+    bgColor: '#fafafa',
+    user: {
+      _id: '123',
+      name: 'Memo',
+    }
+  }];
+
   return (
     <>
       <NavBar />
+
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 'calc( 100vh - 80px' }}
+      />
+
+
     </>
   )
 }
