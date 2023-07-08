01ad197606ead35cd7dc9bf1f507e85443946ef1
Guillermo Sandoval <tinta.mas.toner.mzt@gmail.com>
2023-07-07 17:50:18 -0700

    CalendarPage.jsx Calendario pasado a español

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,47 +1,57 @@
-import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
+import { Calendar } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
-import enUS from 'date-fns/locale/en-US';
-import { addHours, parse, format, startOfWeek, getDay } from 'date-fns';
+import { localizer, getMessagesES } from '../../helpers';
 
+import { addHours } from 'date-fns';
 import { NavBar } from '../components/NavBar';
 
+const events = [{
+  title: 'Cumpleaños del jefe',
+  notes: 'Hay que comprar el pastel',
+  start: new Date(),
+  end: addHours( new Date(), 2),
+  bgColor: '#fafafa',
+  user: {
+    _id: '123',
+    name: 'Memo',
+  }
+}];
 
 export const CalendarPage = () => {
 
-  const locales = {
-    'en-US': enUS,
-  };
+  const eventStyleGetter = ( event, start, end, isSelected ) => {
+    console.log({event, start, end, isSelected});
 
-  const localizer = dateFnsLocalizer({
-    format,
-    parse,
-    startOfWeek,
-    getDay,
-    locales,
-  });
-
-  const events = [{
-    title: 'Cumpleaños del jefe',
-    notes: 'Hay que comprar el pastel',
-    start: new Date(),
-    end: addHours( new Date(), 2),
-    bgColor: '#fafafa',
-    user: {
-      _id: '123',
-      name: 'Memo',
+    const style = {
+      backgroundColor: '#347CF7',
+      borderRadius: '0px',
+      opacity: 0.8,
+      color: 'white',
     }
-  }];
+
+    return {
+      style
+    }
+
+  }
+
+ 
+
+
 
   return (
     <>
       <NavBar />
 
       <Calendar
+        culture="es"
         localizer={localizer}
         events={events}
         startAccessor="start"
         endAccessor="end"
         style={{ height: 'calc( 100vh - 80px' }}
+        messages={ getMessagesES() }
+        eventPropGetter={ eventStyleGetter }
       />
 
 
