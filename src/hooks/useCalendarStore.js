b15d43c8e2aa08d0e1eca5743c84df0b9df29f8b
Guillermo Sandoval <tinta.mas.toner.mzt@gmail.com>
2023-07-10 14:53:17 -0700

    FabDelete.jsx Eliminando eventos

--- src/hooks/useCalendarStore.js
+++ src/hooks/useCalendarStore.js
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { onAddNewEvent, onSetActiveEvent, onUpdateEvent } from '../store';
+import { onAddNewEvent, onDeleteEvent, onSetActiveEvent, onUpdateEvent } from '../store';
 
 
 export const useCalendarStore = () => {
@@ -25,15 +25,22 @@ export const useCalendarStore = () => {
 
    }
 
+   const startDeletingEvent = () => {+
+      //TODO: llegar al backend
+      dispatch( onDeleteEvent() );
+   }
+
 
   return {
         //* Propiedades
         events,
         activeEvent,
+        hasEventSelected: !!activeEvent,
 
         //* Metodos
         setActiveEvent,
         startSavingEvent,
+        startDeletingEvent,
   }
     
 }
