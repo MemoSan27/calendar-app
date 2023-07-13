5189584ee36d514c29c8543743f4545be9b168eb
Guillermo Sandoval <tinta.mas.toner.mzt@gmail.com>
2023-07-13 11:18:40 -0700

    LoginPage.jsx Seccion de login y registro conectado con backend y funcionando

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -18,7 +18,7 @@ const registerFormFields = {
 
 export const LoginPage = () => {
 
-    const { startLogin, errorMessage } = useAuthStore();
+    const { startLogin, startRegister, errorMessage } = useAuthStore();
 
     const { loginEmail, loginPassword, onInputChange: onLoginInputChange } = useForm( loginFormFields );
    const { registerName, registerEmail, registerPassword, registerPassword2, onInputChange: onRegInputChange } = useForm( registerFormFields );
@@ -32,7 +32,11 @@ export const LoginPage = () => {
 
    const registerSubmit = (event) => {
     event.preventDefault();
-    console.log({ registerName, registerEmail, registerPassword, registerPassword2 });
+    if( registerPassword !== registerPassword2 ){
+        Swal.fire('Error en registro', 'Contraseñas no son iguales', 'error');
+        return;
+    }
+    startRegister({ name: registerName, email: registerEmail, password: registerPassword });
 }
 
 useEffect(() => {
