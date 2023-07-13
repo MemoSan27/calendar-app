5550adf9d459576574f80b053780dcc1d2073161
Guillermo Sandoval <tinta.mas.toner.mzt@gmail.com>
2023-07-13 10:45:55 -0700

    LoginPage.jsx Seccion de registro conectado con backend y funcionando

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,5 +1,7 @@
+import { useEffect } from 'react';
 import { useAuthStore, useForm } from '../../hooks';
 import './LoginPage.css';
+import Swal from 'sweetalert2';
 
 const loginFormFields = {
     loginEmail: '',
@@ -16,7 +18,7 @@ const registerFormFields = {
 
 export const LoginPage = () => {
 
-    const { startLogin } = useAuthStore();
+    const { startLogin, errorMessage } = useAuthStore();
 
     const { loginEmail, loginPassword, onInputChange: onLoginInputChange } = useForm( loginFormFields );
    const { registerName, registerEmail, registerPassword, registerPassword2, onInputChange: onRegInputChange } = useForm( registerFormFields );
@@ -32,6 +34,14 @@ export const LoginPage = () => {
     event.preventDefault();
     console.log({ registerName, registerEmail, registerPassword, registerPassword2 });
 }
+
+useEffect(() => {
+  if( errorMessage !== undefined ){
+    Swal.fire('Error en la autenticacion', errorMessage, 'error');
+  }
+
+  
+}, [errorMessage])
 
     return (
         <div className="container login-container">
