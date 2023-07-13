2905e2e5401c6d05d263725e9fd128caa0d50106
Guillermo Sandoval <tinta.mas.toner.mzt@gmail.com>
2023-07-13 12:07:13 -0700

    NavBar.jsx Login, logout y nombre de usuario implementados

--- src/hooks/useAuthStore.js
+++ src/hooks/useAuthStore.js
@@ -43,6 +43,27 @@ export const useAuthStore = () => {
         }
     }
 
+    const checkAuthToken = async() => {
+        const token = localStorage.getItem('token');
+
+        if( !token ) return dispatch( onLogout() );
+
+        try {
+            const { data } = await calendarApi.get('auth/renew');
+            localStorage.setItem('token', data.token);
+            localStorage.setItem('token-init-date', new Date().getTime() );
+            dispatch( onLogin({ name: data.name, uid: data.uid }));
+        } catch (error) {
+            localStorage.clear();
+            dispatch( onLogout() );    
+        }
+    }
+
+    const startLogout = () => {
+        localStorage.clear();
+        dispatch(onLogout());
+    }
+
 
 
     return{
@@ -53,7 +74,9 @@ export const useAuthStore = () => {
         
 
         //*Metodos
+        checkAuthToken,
         startLogin,
         startRegister,
+        startLogout,
     }
 }
